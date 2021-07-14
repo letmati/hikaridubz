aa3f91232611f7fa21fbce3ead333cdd24f68a77
meanwhilematt <86536481+meanwhilematt@users.noreply.github.com>
2021-07-14 05:59:52 -0300

    config sw

--- js/load.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("./js/sw.js")
-        .then((reg) => console.log(reg))
-        .catch((err) => console.log(err));
-}
